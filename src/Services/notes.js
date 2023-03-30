63961e65c0d7438fc58704aeb50543683c7cf355
Karen Sarai Morales Montiel <karen.sarai.morales@gmail.com>
2023-03-29 18:55:59 -0600

    Update notes.js

--- src/Services/notes.js
+++ src/Services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const url = `http://localhost:3001/api/notes`
+const url = `https://note-service-private.onrender.com`
 
 const getAll = () => {
     const request = axios.get(url)
